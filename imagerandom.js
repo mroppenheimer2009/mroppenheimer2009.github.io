298ad8ae0a78ad05fde4c0f8d65ad6e1a9d98c4f
mroppenheimer2009 <146129999+mroppenheimer2009@users.noreply.github.com>
2024-01-21 16:32:27 -0600

    Update imagerandom.js

--- imagerandom.js
+++ imagerandom.js
@@ -2,6 +2,21 @@
 
 <script>
 document.getElementById("demo").innerHTML = Math.random();
+
+if (demo < 0.33333) {
+<td><img height="251" width="251" src="https://web.archive.org/web/20240121173750if_/https://litter.catbox.moe/fi9ror.jpg"align="right"></td>
+</tr></table></div>;
+ } else if (demo > 0.66666) {
+  <td><img height="251" width="251" src="https://web.archive.org/web/20240121222046if_/https://litter.catbox.moe/04zuzw.jpg"align="right"></td>
+</tr></table></div>;
+} else {
+  <td><img height="251" width="251" src="https://web.archive.org/web/20240121222246if_/https://litter.catbox.moe/zw9ou6.jpg"align="right"></td>
+</tr></table></div>;
+}
+
+
+
+
 </script>
 
 </body>
