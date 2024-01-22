44d6205bd0888fe1f74cb9051f98ab849289dec0
mroppenheimer2009 <146129999+mroppenheimer2009@users.noreply.github.com>
2024-01-21 18:14:36 -0600

    Update imagerandom.js

--- imagerandom.js
+++ imagerandom.js
@@ -1,21 +1,18 @@
 <p id="demo"></p>
 
 <script>
-document.getElementById("demo").innerHTML = Math.random();
 
-if (demo < 0.33333) {
-<td><img height="251" width="251" src="https://web.archive.org/web/20240121173750if_/https://litter.catbox.moe/fi9ror.jpg"align="right"></td>
-</tr></table></div>;
- } else if (demo > 0.66666) {
-  <td><img height="251" width="251" src="https://web.archive.org/web/20240121222046if_/https://litter.catbox.moe/04zuzw.jpg"align="right"></td>
-</tr></table></div>;
-} else {
-  <td><img height="251" width="251" src="https://web.archive.org/web/20240121222246if_/https://litter.catbox.moe/zw9ou6.jpg"align="right"></td>
-</tr></table></div>;
+
+var images = [
+'https://web.archive.org/web/20240121173750if_/https://litter.catbox.moe/fi9ror.jpg',
+'https://web.archive.org/web/20240121222046if_/https://litter.catbox.moe/04zuzw.jpg',
+'https://web.archive.org/web/20240121222246if_/https://litter.catbox.moe/zw9ou6.jpg'
+        ];
+
+function finallyBro(){
+var numbles = Math.floor(Math.random()* (images.length));
+document.getElementById('imageDisplay').innerHTML = images[randomNumber];
 }
-
-
-
 
 </script>
 
